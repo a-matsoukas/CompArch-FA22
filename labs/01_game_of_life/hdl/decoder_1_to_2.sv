1313ff4dcf26974342cd0aa34ac45db2cc56d044
Alex Matsoukas <amatsoukas@olin.edu>
2022-09-28 20:54:54 -0400

    add code for 1:2 decoder from class

--- labs/01_game_of_life/hdl/decoder_1_to_2.sv
+++ labs/01_game_of_life/hdl/decoder_1_to_2.sv
@@ -4,5 +4,11 @@ input wire ena;
 input wire in;
 output logic [1:0] out;
 
+logic in_bar;
+always_comb begin
+    out[1] = in & ena;
+    in_bar = ~in;
+    out[0] = in_bar & ena; // Alternate: out[0] = ~in & ena;
+end
 
 endmodule
