56cf7c12f29f53f8c917fe4336b78470011c881d
Alex Matsoukas <amatsoukas@olin.edu>
2022-10-03 19:46:29 -0400

    implemented decoder_2_to_4.sv, but have not tested

--- labs/01_game_of_life/hdl/decoder_2_to_4.sv
+++ labs/01_game_of_life/hdl/decoder_2_to_4.sv
@@ -5,5 +5,13 @@ input wire ena;
 input wire [1:0] in;
 output logic [3:0] out;
 
+logic mid;
+
+always_comb begin
+    decoder_1_to_2 first_decoder(ena, in[1], mid);
+    decoder_1_to_2 second_decoder0(mid[0], in[0], out[1:0]);
+    decoder_1_to_2 second_decoder1(mid[1], in[0], out[3:2]);   
+end
+
 
 endmodule
