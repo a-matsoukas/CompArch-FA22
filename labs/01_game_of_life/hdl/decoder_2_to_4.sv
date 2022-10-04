802c58dcc0799a25bd2c61e4727bf1ef671de3b0
Alex Matsoukas <amatsoukas@olin.edu>
2022-10-03 21:27:25 -0400

    resolve syntax errors in decoder_2_to_4.sv

--- labs/01_game_of_life/hdl/decoder_2_to_4.sv
+++ labs/01_game_of_life/hdl/decoder_2_to_4.sv
@@ -5,13 +5,10 @@ input wire ena;
 input wire [1:0] in;
 output logic [3:0] out;
 
-logic mid;
+logic [1:0] mid;
 
-always_comb begin
-    decoder_1_to_2 first_decoder(ena, in[1], mid);
-    decoder_1_to_2 second_decoder0(mid[0], in[0], out[1:0]);
-    decoder_1_to_2 second_decoder1(mid[1], in[0], out[3:2]);   
-end
-
+decoder_1_to_2 first_decoder(ena, in[1], mid);
+decoder_1_to_2 second_decoder0(mid[0], in[0], out[1:0]);
+decoder_1_to_2 second_decoder1(mid[1], in[0], out[3:2]);
 
 endmodule
