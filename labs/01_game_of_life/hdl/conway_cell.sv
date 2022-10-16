d10e37d89a298124091143a4706440e42395a775
lila <lsmith2@olin.edu>
2022-10-16 19:55:34 -0400

    Updated usage and state_d and added enable

--- labs/01_game_of_life/hdl/conway_cell.sv
+++ labs/01_game_of_life/hdl/conway_cell.sv
@@ -16,29 +16,33 @@ input wire [7:0] neighbors;
 logic [3:0] sum;
 logic c_out;
 
-wire [35:0] sum_memory;
-assign sum_memory[3:0] = 4'b0;
-assign sum = sum_memory[35:32];
+logic [35:0] sum_memory;
+
+always_comb begin
+    sum_memory[3:0] = 4'b0;
+    sum[3:0] = sum_memory[35:32];
+end
 
 wire [7:0] carries_out;
 
 generate
     genvar i;
     for(i = 0; i < 8; i++) begin
-        // tmp = 4'b1 & neighbors[i];
-        // assign tmp = 4'b1; // neighbors[i] == 1 ? 4'b1 : 4'b0;
         adder_n #(.N(4)) adder_4bit (
             .a(sum_memory[i*4 + 3 : i*4]),
-            .b(neighbors[i]),
+            .b({3'b0, neighbors[i]}),
             .c_in(1'b0),
             .sum(sum_memory[(i + 1)*4 + 3 : (i + 1)*4]),
             .c_out(carries_out[i]));
     end
 endgenerate
 
+always_comb begin
+    state_d = rst ? state_0 : (sum == 3) | (state_q & (sum == 2));
+end
+
 always_ff @( posedge clk ) begin
-    state_q <= ena & ~rst & ((sum == 3) | (state_0 & (sum == 2)));
-    state_d <= state_q;
+    state_q <= ena ? state_d : state_q;
 end
 
 
