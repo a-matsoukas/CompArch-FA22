61649ff74086ab0e03ef686d497e8a6ac73be16a
Alex Matsoukas <amatsoukas@olin.edu>
2022-11-01 18:59:31 -0400

    finish pulse generator

--- labs/02_etch_a_sketch/hdl/pulse_generator.sv
+++ labs/02_etch_a_sketch/hdl/pulse_generator.sv
@@ -9,8 +9,23 @@ input wire clk, rst, ena;
 input wire [N-1:0] ticks;
 output logic out;
 
-logic [N-1:0] counter;
-logic counter_comparator;
+logic [N-1:0] counter, counter_next;
+logic local_rst;
+
+always_comb begin
+  counter_next = counter + 1;
+  out = counter == ticks;
+  local_rst = rst | out;
+end
+
+always_ff @(posedge clk) begin : increment_counter
+  if (local_rst) begin // since the counter starts at 0, subtract one from ticks
+    counter <= 0;
+  end
+  else if (ena) begin
+      counter <= counter_next;
+  end
+end
 
 
 endmodule
