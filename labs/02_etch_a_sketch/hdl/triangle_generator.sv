4970699c7f7a2864096be26eb3f952e8a2a1852f
lila <lsmith2@olin.edu>
2022-11-01 19:51:28 -0400

    Add triangle_generator and waves for it

--- labs/02_etch_a_sketch/hdl/triangle_generator.sv
+++ labs/02_etch_a_sketch/hdl/triangle_generator.sv
@@ -6,8 +6,61 @@ parameter N = 8;
 input wire clk, rst, ena;
 output logic [N-1:0] out;
 
-typedef enum logic {COUNTING_UP, COUNTING_DOWN} state_t;
-state_t state;
+typedef enum logic {COUNTING_UP = 1'b0, COUNTING_DOWN =  1'b1} state_t;
+state_t state, next_state;
+
+logic counter_zero, counter_max;
+logic [N-1:0] increment, out_next;
+
+
+always_comb begin : state_comb
+    counter_zero = ~|(out_next);
+    counter_max = &(out_next);
+    case (state)
+        COUNTING_UP   : begin
+            if (counter_max) begin
+                next_state = COUNTING_DOWN;
+            end 
+            else begin
+                next_state = COUNTING_UP;
+            end
+        end
+        COUNTING_DOWN : begin
+            if (counter_zero) begin
+                next_state = COUNTING_UP;
+            end
+            else begin
+                next_state = COUNTING_DOWN;
+            end
+        end
+    endcase
+end
+
+always_comb begin : counter_comb
+    case (state)
+        COUNTING_UP   : increment = 1;
+        COUNTING_DOWN : increment = -1;
+    endcase
+    out_next = out + increment;
+end
+
+always_ff @(posedge clk) begin : counter
+    if (rst) begin
+        out <= 0;
+    end
+    else if (ena) begin
+        out <= out_next;
+    end    
+end
+
+always_ff @(posedge clk) begin : state_machine
+    if (rst) begin
+        state <= COUNTING_UP;
+    end 
+    else begin
+        state <= next_state;
+    end
+end
 
 
 endmodule
